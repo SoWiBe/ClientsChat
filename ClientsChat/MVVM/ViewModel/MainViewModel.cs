ab2d02d70185692e0c20d7f29b64ee8096cf6646
Aleksey <aleksey.dantist@gmail.com>
2022-05-30 12:30:49 +0600

    new comments

--- ClientsChat/MVVM/ViewModel/MainViewModel.cs
+++ ClientsChat/MVVM/ViewModel/MainViewModel.cs
@@ -13,25 +13,35 @@ using System.Windows.Input;
 
 namespace ClientsChat.MVVM.ViewModel
 {
+    //Модель, которая описывает окно чата
     class MainViewModel : ObservableObject
     {
+        //Объявления всех коллекций
         public ObservableCollection<UserModel> Users { get; set; }
         public ObservableCollection<MessageModel> Messages { get; set; }
         public ObservableCollection<Clients> Clients { get; set; }
         public ObservableCollection<Question> Questions { get; set; }
+        //Имя для входа в чат
         public string Username { get; set; }
 
         /* Commands */
+
+        //Команда на отправку
         public RelayCommand SendCommand { get; set; }
+        //Команда на соединение с сервером
         public RelayCommand ConnectToServerCommand { get; set; }
+        //Команда на отправку сообщения
         public RelayCommand SendMessageCommand { get; set; }
+        //Команда на открытие чата
         public ICommand OpenChatPage { get; set; }
+        //Команда на закрытие вопроса
         public ICommand CloseQuestionCommand { get; set; }
 
+        //Сервер
         private Server _server;
-
+        //Выбранный контакт
         private Clients _selectedContact;
-
+        //Свойство, для получения или установления выбранного контакта
         public Clients SelectedContact
         {
             get { return _selectedContact; }
@@ -41,9 +51,9 @@ namespace ClientsChat.MVVM.ViewModel
                 OnPropertyChanged();
             }
         }
-
+        //Выбранный вопрос
         private Question _selectedQuestion;
-
+        //Свойство, для получения или установления выбранного вопроса
         public Question SelectedQuestion
         {
             get { return _selectedQuestion; }
@@ -53,33 +63,36 @@ namespace ClientsChat.MVVM.ViewModel
                 OnPropertyChanged();
             }
         }
-
+        //Поле сообщения
         public string Message { get; set; }
         
         public MainViewModel()
         {
             Users = new ObservableCollection<UserModel>();
             _server = new Server();
+
+            //Привязка событий
             _server.connectedEvent += UserConnected;
             _server.msgReceivedEvent += MessageReceived;
             _server.userDisconnectEvent += RemoveUser;
 
+            //Установка комманд
             ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer(Username), o => !string.IsNullOrEmpty(Username));
             SendMessageCommand = new RelayCommand(o => _server.SendMessageToServer(Message), o => !string.IsNullOrEmpty(Message));
             CloseQuestionCommand = new RelayCommand(o => CloseQuestion(), o => SelectedQuestion != null);
             
-
+            //Выделение памяти под коллекции
             Messages = new ObservableCollection<MessageModel>();
             Clients = new ObservableCollection<Clients>();
             Questions = new ObservableCollection<Question>();
 
-
+            //Вывод вопросов с определенным статусом
             SetAndUpdateQuestions();
 
+            //Установка первого вопроса, как выбранного
             if (Questions.Count != 0)
             {
                 SelectedQuestion = Questions.First();
-
             }
         }
 
@@ -92,6 +105,8 @@ namespace ClientsChat.MVVM.ViewModel
                 Questions.Add(listQuestions.ElementAt(i));
             }
         }
+
+        //Закрытие вопроса
         private void CloseQuestion()
         {
             Question question = SelectedQuestion;
@@ -101,16 +116,14 @@ namespace ClientsChat.MVVM.ViewModel
             ClientChatEntities.GetContext().SaveChanges();
             MessageBox.Show("Вопрос по теме: " + question.Info + " был закрыт!", "Вопрос открыт!", MessageBoxButton.OK);
             SetAndUpdateQuestions();
-            
         }
-
+        //Отсоединение пользователя с чата
         private void RemoveUser()
         {
             var uid = _server.PacketReader.ReadMessage();
             var user = Users.Where(x => x.ID == uid).FirstOrDefault();
             Application.Current.Dispatcher.Invoke(() => Users.Remove(user));
         }
-
         private void MessageReceived()
         {
             var msg = _server.PacketReader.ReadMessage();
@@ -121,7 +134,7 @@ namespace ClientsChat.MVVM.ViewModel
                 IsNativeOrigin = true
             }));
         }
-
+        //Присоединение пользователсся к чату
         private void UserConnected()
         {
             var user = new UserModel
